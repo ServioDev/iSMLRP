5f5c03fc641874aa85477c57be25ecf37416245f
Annoyimous <jeromejunio@outlook.com>
2021-11-03 13:49:00 +0800

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyDoFnDKtXFj4sYTCRmtZLIrtHfUfYtBxUI',
+  authDomain: 'ismlrp-auth-dev-fddb3.firebaseapp.com',
+  databaseURL:
+    'https://ismlrp-auth-dev-fddb3-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'ismlrp-auth-dev-fddb3',
+  storageBucket: 'ismlrp-auth-dev-fddb3.appspot.com',
+  messagingSenderId: '461713266912',
+  appId: '1:461713266912:web:37b4a5d05fac4e1f01d885',
 });
 
 export const auth = app.auth();
